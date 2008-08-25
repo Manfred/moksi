22c39a23ca826475303d564e14246cde363fc486
Manfred Stienstra <manfred@fngtps.com>
2008-08-25 22:51:23 +0200

    Implement stubbing.

--- javascripts/moksi.js
+++ javascripts/moksi.js
@@ -1,2 +1,40 @@
 var Moksi = {
+  stubbed: [],
+  originalMethodRegexp: /^__before_stub_(.*)$/,
+  
+  temporaryNameFor: function(functionName) {
+    return "__before_stub_" + functionName;
+  },
+  
+  stub: function(object, functionName, definition) {
+    var temporaryName = this.temporaryNameFor(functionName);
+    
+    object[temporaryName] = object[functionName];
+    object[functionName] = definition;
+    
+    if (this.stubbed.indexOf(object) == -1) {
+      this.stubbed.push(object);
+    }
+  },
+  
+  unstub: function(object, functionName, temporaryName) {
+    if (!temporaryName) temporaryName = this.temporaryNameFor(functionName);
+    object[functionName] = object[temporaryName];
+    delete object[temporaryName];
+  },
+  
+  revert: function() {
+    var object;
+    while(object = this.stubbed.pop()) {
+      var keys = Object.keys(object);
+      for (i=0; i < keys.length; i++) {
+        var name = keys[i];
+        var match;
+        if (match = name.match(this.originalMethodRegexp)) {
+          object[match[1]] = object[name];
+          delete object[name];
+        }
+      }
+    }
+  }
 };
--- javascripts/moksi_test.js
+++ javascripts/moksi_test.js
@@ -1,5 +1,40 @@
+var Person = {
+  name: function() {
+    return 'Alice';
+  },
+  age: function() {
+    return 21;
+  }
+};
+
 new Test.Unit.Runner({
-  testTruth: function(){
-    this.assert(true);
+  setup: function() {
+    Moksi.stub(Person, 'name', function() {
+      return 'Bob';
+    });
+    Moksi.stub(Person, 'age', function() {
+      return 28;
+    });
+  },
+  
+  teardown: function() {
+    Moksi.revert();
+  },
+  
+  testStub: function() {
+    this.assertEqual('Bob', Person.name());
+    this.assertEqual(28, Person.age());
+  },
+  
+  testRevert: function() {
+    Moksi.revert();
+    this.assertEqual('Alice', Person.name());
+    this.assertEqual(21, Person.age());
+  },
+  
+  testUnStub: function() {
+    Moksi.unstub(Person, 'name');
+    this.assertEqual('Alice', Person.name());
+    this.assertEqual(28, Person.age());
   }
 });
