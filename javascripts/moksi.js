f686a532366c0ecd5485377653c250681a680ed5
Manfred Stienstra <manfred@fngtps.com>
2008-08-26 15:34:59 +0200

    Implement expectations.

--- javascripts/moksi.js
+++ javascripts/moksi.js
@@ -1,13 +1,17 @@
 var Moksi = {
   stubbed: [],
-  originalMethodRegexp: /^__before_stub_(.*)$/,
+  called: {},
+  expected: {},
   
-  temporaryNameFor: function(functionName) {
-    return "__before_stub_" + functionName;
+  beforeStubPrefix: "__before_stub_",
+  beforeStubRegexp: /^__before_stub_(.*)$/,
+  
+  stubbedFunctionName: function(functionName) {
+    return this.beforeStubPrefix + functionName;
   },
   
   stub: function(object, functionName, definition) {
-    var temporaryName = this.temporaryNameFor(functionName);
+    var temporaryName = this.stubbedFunctionName(functionName);
     
     object[temporaryName] = object[functionName];
     object[functionName] = definition;
@@ -17,23 +21,57 @@ var Moksi = {
     }
   },
   
-  unstub: function(object, functionName, temporaryName) {
-    if (!temporaryName) temporaryName = this.temporaryNameFor(functionName);
-    object[functionName] = object[temporaryName];
-    delete object[temporaryName];
+  unstub: function(object, functionName) {
+    var temporaryName = this.stubbedFunctionName(functionName);
+    this.revertMethod(object, functionName, temporaryName);
+  },
+  
+  expects: function(object, functionName) {
+    this.expected[object] = this.expected[object] || {}
+    this.expected[object][functionName] = 1;
+    
+    this.stub(object, functionName, function() {
+      Moksi.called[object] = Moksi.called[object] || {};
+      Moksi.called[object][functionName] = Moksi.called[object][functionName] || 0;
+      Moksi.called[object][functionName] += 1;
+    });
   },
   
   revert: function() {
     var object;
     while(object = this.stubbed.pop()) {
-      var keys = Object.keys(object);
-      for (i=0; i < keys.length; i++) {
-        var name = keys[i];
+      for (var property in object) {
         var match;
-        if (match = name.match(this.originalMethodRegexp)) {
-          object[match[1]] = object[name];
-          delete object[name];
+        if (match = property.match(this.beforeStubRegexp)) {
+          this.revertMethod(object, match[1], property);
         }
+      }
+    }
+    this.expected = {};
+    this.called = {};
+  },
+  
+  revertMethod: function(object, originalName, temporaryName) {
+    object[originalName] = object[temporaryName];
+    delete object[temporaryName];
+  },
+  
+  assertExpectations: function(testCase) {
+    for (object in this.expected) {
+      for (property in this.expected[object]) {
+        var called;
+        if ((this.called[object]) && (this.called[object][property])) {
+          called = this.called[object][property];
+        } else {
+          called = 0;
+        }
+        
+        var message = testCase.buildMessage('expectation',
+          'expected ?.? to be called ? times, but was called ? times',
+          object.name||'Object',
+          property,
+          this.expected[object][property], called);
+        testCase.assertEqual(this.expected[object][property], called, message);
       }
     }
   }
--- javascripts/moksi_test.js
+++ javascripts/moksi_test.js
@@ -7,14 +7,14 @@ var Person = {
   }
 };
 
-new Test.Unit.Runner({
+new Test.Unit.Runner({  
   setup: function() {
     Moksi.stub(Person, 'name', function() {
       return 'Bob';
     });
     Moksi.stub(Person, 'age', function() {
       return 28;
-    });
+    });    
   },
   
   teardown: function() {
@@ -32,9 +32,66 @@ new Test.Unit.Runner({
     this.assertEqual(21, Person.age());
   },
   
-  testUnStub: function() {
+  testUnstub: function() {
     Moksi.unstub(Person, 'name');
     this.assertEqual('Alice', Person.name());
     this.assertEqual(28, Person.age());
+  },
+  
+  testExpects: function() {
+    Moksi.expects(Person, 'name');
+    this.assertEqual(null, Person.name());
+  },
+  
+  testAssertExpectationsSucceeds: function() {
+    Moksi.expects(Person, 'name');
+    
+    Person.name();
+    
+    Moksi.assertExpectations(this);
+  },
+  
+  testAssertExpectationsNotCalled: function() {
+    var mockTestCase = {
+      assertEqualArguments: null,
+      assertEqual: function() {
+        this.assertEqualArguments = this.assertEqual.arguments;
+      },
+      buildMessage: function() {
+        return 'built message';
+      }
+    };
+    
+    Moksi.expects(Person, 'name');
+    
+    Moksi.assertExpectations(mockTestCase);
+    
+    this.assertEqual(1, mockTestCase.assertEqualArguments[0]);
+    this.assertEqual(0, mockTestCase.assertEqualArguments[1]);
+    this.assertEqual('built message', mockTestCase.assertEqualArguments[2]);
+  },
+  
+  testAssertExpectationsCalledTooOften: function() {
+    var mockTestCase = {
+      assertEqualArguments: null,
+      assertEqual: function() {
+        this.assertEqualArguments = this.assertEqual.arguments;
+      },
+      buildMessage: function() {
+        return 'built message';
+      }
+    };
+    
+    Moksi.expects(Person, 'name');
+    
+    Person.name();
+    Person.name();
+    Person.name();
+    
+    Moksi.assertExpectations(mockTestCase);
+    
+    this.assertEqual(1, mockTestCase.assertEqualArguments[0]);
+    this.assertEqual(3, mockTestCase.assertEqualArguments[1]);
+    this.assertEqual('built message', mockTestCase.assertEqualArguments[2]);
   }
 });
