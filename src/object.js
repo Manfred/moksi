3b3a441fc9c1a580c7c8ea2e1871f1604bf391a2
Manfred Stienstra <manfred@fngtps.com>
2009-09-30 22:01:24 +0200

    Implement enumerable equality checking.

--- src/object.js
+++ src/object.js
@@ -1,15 +1,25 @@
 Moksi.Object = {
-  isEqual: function(one, other) {
-    var type = typeof one;
-    switch(type) {
-      case 'object':
-        return isEqualObject(one, other);
-      default:
-        return one == other;
+  isEqual: function(left, right) {
+    if (typeof left.length == 'number' && typeof left != 'string') {
+      return Moksi.Object.isEqualEnumerable(left, right);
+    } else if (typeof left == 'object') {
+      return Moksi.Object.isEqualObject(left, right);
+    } else {
+      return left == right;
     }
   },
   
-  isEqualObject: function(one, other) {
-    return one == other;
+  isEqualEnumerable: function(left, right) {
+    if (left.length != right.length) return false;
+    
+    for(i=0; i < left.length; i++) {
+      if (!this.isEqual(left[i], right[i])) return false;
+    }
+    
+    return true;
+  },
+  
+  isEqualObject: function(left, right) {
+    return left == right;
   }
 };
--- test/unit/object_test.js
+++ test/unit/object_test.js
@@ -13,12 +13,36 @@ Moksi.describe('Moksi.Object', {
   },
   
   'tests base object equality': function() {
-    var examples = [[1, 1], [2, 2], ['one', 'one'], [2.0, 2.0]];
-    expectEqual(examples);
+    expectEqual([[1, 1], [2, 2], ['one', 'one'], [2.0, 2.0]]);
   },
   
   'tests base object inequality': function() {
-    var examples = [[1, 2], [2, 1], ['one', 'two'], [2.0, 1.0]];
-    expectNotEqual(examples);
+    expectNotEqual([[1, 2], [2, 1], ['one', 'two'], [2.0, 1.0]]);
+  },
+  
+  'tests enumerable object equality': function() {
+    expectEqual([
+      [[],[]],
+      [[1],[1]],
+      [[1,2],[1,2]],
+      [[1,2,3],[1,2,3]],
+      [[[1],[2],[3,4]],[[1],[2],[3,4]]],
+      [['a'],['a']],
+      [[0],[0]],
+    ]);
+  },
+  
+  'test enumerable object inequality': function() {
+    expectNotEqual([
+      [[1],[2]],
+      [[1],[]],
+      [[],[1]],
+      [[1,2],[1,2,3]],
+      [[1,2,3],[1,2]],
+      [[1,3,2],[1,2,3]],
+      [[[1],[2],[3,4]],[[1],[2],[3]]],
+      [['a'],['a','b']],
+      [[0, 0],[0]]
+    ]);
   }
 });
